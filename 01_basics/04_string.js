a7c825ad178d2180454149a22431465679fb8000
Anil Bhatt <67644691+anilbhatt094@users.noreply.github.com>
2024-05-06 12:11:45 +0000

    stringfunctionupdate

---
+++ 01_basics/04_string.js
@@ -0,0 +1,84 @@
+const userName =("Anil Bhatt");
+// console.log(userName);
+
+//String Length"
+// const usernamelen = userName.length;
+// console.log (usernamelen);
+
+//String Replace()
+// const usernameRep = userName.replace('Anil' , 'Bhatt');
+// console.log(usernameRep);
+
+//String IndexOf()
+// const userindexof = userName.indexOf("ha");
+// console.log(userindexof);
+
+//String LastIndexOf()
+// const userLastIndex = userName.lastIndexOf("h");
+// console.log(userLastIndex);
+
+//String StartWith()
+// const userStartWith = userName.startsWith("t" , 7);
+// console.log(userStartWith);
+
+//String EndWith()
+// const userEndWith = userName.endsWith("l", 4);
+// console.log(userEndWith);
+
+//String UpperCash()
+// const userUppercash = userName.toUpperCase();
+// console.log(userUppercash);
+
+//String LowerCash()
+// const userlowerCash = userName.toLowerCase();
+// console.log(userlowerCash);
+
+//String Include()
+// const userInclude = userName.includes("Sunil");
+// console.log(userInclude);
+
+//String Repeat()
+// const userRepeat = userName.repeat(4);
+// console.log(userRepeat);
+
+//String CharAt()
+// const userCharAt = userName.charAt(0);
+// console.log(userCharAt);
+
+//String CharCodeAt() - https://asecuritysite.com/coding/asc2
+// const usercharcodeAt = userName.charCodeAt(3);
+// console.log(usercharcodeAt);
+
+//string fromcharcode()
+// const userfromcharcode = String.fromCharCode(65 ,110, 105);
+// console.log(userfromcharcode);
+
+//String Substring()
+// const usersubstring = userName.substring(0,10);
+// console.log(usersubstring);
+
+//String PadStart()
+// const userpadStart = userName.padStart(20, "*");
+// console.log(userpadStart);
+
+//String EndStart()
+// const userendstart = userName.padEnd(20, "*")
+// console.log(userendstart);
+
+//String CodePointAt()
+// const usercodepointat = userName.codePointAt("A");
+// console.log(usercodepointat);
+
+//String FromCodePoint()
+// const usercodepointfrom = String.fromCodePoint(65);
+// console.log(usercodepointfrom);
+
+//string match()
+// const userexp = "Ani"
+// const usermatch = userName.match(userexp);
+// console.log(usermatch);
+
+//string matchAll()
+// const userexp = /Ani\d/g;
+// const userallmatch = userName.matchAll(userexp);
+// console.log(userallmatch);
